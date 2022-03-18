503d91573e02a1c1f2b90d082075f9aac23f72d1
Jackson Alves <jack182@outlook.com.br>
2022-03-18 16:54:06 -0300

    SellerDaoJBDC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,9 +12,10 @@ public class Program {
 	public static void main(String[] args) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "Bob@bob.com", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
